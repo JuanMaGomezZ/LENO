c8e318164d680b96edf2aaa55062dc8240f44667
Juan Manuel Gomez Omil <72764054+JuanchiiGomezZ@users.noreply.github.com>
2022-07-20 13:03:08 -0300

    Add files via upload

---
+++ Galeria/main.js
@@ -0,0 +1,33 @@
+/* Cart */
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
+/* Open Cart*/
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+/* Close Cart */
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+
+/* burgerIcon */
+
+let burgerIcon = document.querySelector("#burger-icon");
+let burgerMenu = document.querySelector("#burger-menu");
+let crossIcon = document.querySelector("#cross-icon");
+/* Open Menu*/
+
+burgerIcon.onclick = () => {
+  burgerMenu.classList.add("active");
+  burgerIcon.classList.add("hide");
+  crossIcon.classList.add("active");
+};
+/* Close Menu */
+crossIcon.onclick = () => {
+  burgerMenu.classList.remove("active");
+  crossIcon.classList.remove("active");
+  burgerIcon.classList.remove("hide");
+};
