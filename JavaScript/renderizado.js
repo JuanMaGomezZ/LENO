a9a68c50ef006052bf1ee8ba4bd294e65214cb8f
Juan Manuel Gomez Omil <72764054+JuanchiiGomezZ@users.noreply.github.com>
2022-07-21 20:23:22 -0300

    Add files via upload

---
+++ JavaScript/renderizado.js
@@ -0,0 +1,245 @@
+const burgerSimples = [
+    {
+      id: 101,
+      nombre: "Cheeseburger",
+      precio: 700,
+      imagen: "/assets/burgers/Simple.png",
+      carne: "Medallon 115gr",
+      queso: "Cheddar x2",
+      topping1: "",
+      topping2: "",
+      topping3: "",
+    },
+    {
+      id: 102,
+      nombre: "Red Peper",
+      precio: 850,
+      imagen: "/assets/burgers/RedPeper.png",
+      carne: "Medallon 115gr",
+      queso: "Cheddar x2",
+      topping1: "Pimiento Asado",
+      topping2: "Cebolla",
+      topping3: "Ketchup y Mostaza",
+    },
+    {
+      id: 103,
+      nombre: "Sleepy Joe",
+      precio: 850,
+      imagen: "/assets/burgers/SleepyJoe.png",
+      carne: "Medallon 115gr",
+      queso: "Cheddar x4",
+      topping1: "Salsa SleepyJoe",
+      topping2: "Mayonesa American Style",
+      topping3: "",
+    },
+  ];
+  
+  const burgerDobles = [
+    {
+      id: 104,
+      nombre: "Big Leno",
+      precio: 800,
+      imagen: "/assets/burgers/BigLeno.png",
+      carne: "Medallon 90gr x2",
+      queso: "Cheddar x2",
+      topping1: "Cebolla",
+      topping2: "Lechuga",
+      topping3: "Pepinillos",
+      topping4: "Salsa Big LENO",
+    },
+    {
+      id: 105,
+      nombre: "Double Double",
+      precio: 800,
+      imagen: "/assets/burgers/DoubleDouble.png",
+      carne: "Medallon 90gr x2",
+      queso: "Cheddar x2",
+      topping1: "Cebolla en rodaja",
+      topping2: "Tomate",
+      topping3: "Lechuga",
+      topping4: "Salsa LENO",
+    },
+    {
+      id: 106,
+      nombre: "Uncle Sam",
+      precio: 950,
+      imagen: "/assets/burgers/UncleSamXL.png",
+      carne: "Medallon 90gr x2",
+      queso: "Cheddar x2",
+      topping1: "Bacon",
+      topping2: "Cebolla",
+      topping3: "Pepinillos",
+      topping4: "Salsa LENO",
+    },
+    {
+      id: 107,
+      nombre: "Oklahoma",
+      precio: 800,
+      imagen: "/assets/burgers/Oklahoma.png",
+      carne: "Medallon 90gr x2",
+      queso: "Cheddar x2",
+      topping1: "Cebolla fried",
+      topping2: "Pepinillos",
+      topping3: "Mayonesa American",
+      topping4: "",
+    },
+    {
+      id: 108,
+      nombre: "Leno Mole",
+      precio: 950,
+      imagen: "/assets/burgers/LenoMole.png",
+      carne: "Medallon 115gr x2",
+      queso: "Cheddar x4",
+      topping1: "Bacon bits",
+      topping2: "Guacamole",
+      topping3: "Lechuga",
+      topping4: "Mayonesa HOT",
+    },
+    {
+      id: 109,
+      nombre: "Elementary Crispy",
+      precio: 950,
+      imagen: "/assets/burgers/ElementaryCrispy.png",
+      carne: "Medallon 90gr x2",
+      queso: "Cheddar x2",
+      topping1: "Bacon",
+      topping2: "Cebolla crispy",
+      topping3: "Salsa Barbacoa",
+      topping4: "",
+    },
+    {
+      id: 110,
+      nombre: "American Burger",
+      precio: 1000,
+      imagen: "/assets/burgers/AmericanBurger.png",
+      carne: "Medallon 90gr x2",
+      queso: "Cheddar x4",
+      topping1: "Bacon",
+      topping2: "Cebolla",
+      topping3: "Tomate y Lechuga",
+      topping4: "Salsa the American",
+    },
+  ];
+  
+  const burgerTriples = [
+    {
+      id: 111,
+      nombre: "Triple Q",
+      precio: 800,
+      imagen: "/assets/burgers/TripleQ.png",
+      carne: "Medallon 75gr x3 Smashed",
+      queso: "Cheddar x3",
+      topping1: "Cebolla Brunoise",
+      topping2: "Ketchup LENO",
+      topping3: "",
+      topping4: "",
+    },
+    {
+      id: 112,
+      nombre: "345",
+      precio: 1000,
+      imagen: "/assets/burgers/345.png",
+      carne: "Medallon 115gr x3",
+      queso: "Cheddar x6",
+      topping1: "Cebolla Brunoise",
+      topping2: "Pepinillos",
+      topping3: "Ketchup LENO",
+      topping4: "",
+    },
+    {
+      id: 113,
+      nombre: "Uncle Sam XL",
+      precio: 1100,
+      imagen: "/assets/burgers/UncleSamXL.png",
+      carne: "Medallon 90gr x3",
+      queso: "Cheddar x6",
+      topping1: "Bacon",
+      topping2: "Cebolla Brunoise",
+      topping3: "Pepinillos",
+      topping4: "Salsa LENO",
+    },
+    {
+      id: 114,
+      nombre: "Grandpa Sam",
+      precio: 1250,
+      imagen: "/assets/burgers/GrandpaSam.png",
+      carne: "Medallon 90gr x4",
+      queso: "Cheddar x8",
+      topping1: "Bacon",
+      topping2: "Cebolla a la manteca",
+      topping3: "Mayonesa con Relish",
+      topping4: "Salsa LENO",
+    },
+  ];
+
+  function IniciarProductos1() {
+    let divShop1 = document.createElement("div");
+    divShop1.classList.add("shop-content");  
+    let simples = document.getElementById("simples");
+    burgerSimples.forEach((producto) => {
+     
+      let divProducto1 = document.createElement("div");
+      divProducto1.classList.add("product-box");
+      divProducto1.innerHTML = `<img src="${producto.imagen}" alt="" class="product-img">
+                              <h2 class="product-title">${producto.nombre}</h2>
+                              <p class="product-description">${producto.carne}</p>
+                              <p class="product-description">${producto.queso}</p>
+                              <p class="product-description">${producto.topping1}</p>
+                              <p class="product-description">${producto.topping2}</p>
+                              <p class="product-description">${producto.topping3}</p>
+                              <span class="price">$ ${producto.precio}</span>
+                              <i class="fa-solid fa-cart-plus add-cart"></i>`;
+                              
+      divShop1.appendChild(divProducto1);
+      simples.appendChild(divShop1);
+    });
+  }
+  IniciarProductos1();
+  
+  
+  function IniciarProductos2() {
+    let divShop2 = document.createElement("div");
+    divShop2.classList.add("shop-content");  
+    let dobles = document.getElementById("dobles");
+    burgerDobles.forEach((producto) => {
+     
+      let divProducto2 = document.createElement("div");
+      divProducto2.classList.add("product-box");
+      divProducto2.innerHTML = `<img src="${producto.imagen}" alt="" class="product-img">
+                              <h2 class="product-title">${producto.nombre}</h2>
+                              <p class="product-description">${producto.carne}</p>
+                              <p class="product-description">${producto.queso}</p>
+                              <p class="product-description">${producto.topping1}</p>
+                              <p class="product-description">${producto.topping2}</p>
+                              <p class="product-description">${producto.topping3}</p>
+                              <span class="price">$ ${producto.precio}</span>
+                              <i class="fa-solid fa-cart-plus add-cart"></i>`;
+                              
+      divShop2.appendChild(divProducto2);
+      dobles.appendChild(divShop2);
+    });
+  }
+  IniciarProductos2();
+  
+  function IniciarProductos3() {
+    let divShop3 = document.createElement("div");
+    divShop3.classList.add("shop-content");  
+    let triples = document.getElementById("triples");
+    burgerTriples.forEach((producto) => {
+      let divProducto3 = document.createElement("div");
+      divProducto3.classList.add("product-box");
+      divProducto3.innerHTML = `<img src="${producto.imagen}" alt="" class="product-img">
+                              <h2 class="product-title">${producto.nombre}</h2>
+                              <p class="product-description">${producto.carne}</p>
+                              <p class="product-description">${producto.queso}</p>
+                              <p class="product-description">${producto.topping1}</p>
+                              <p class="product-description">${producto.topping2}</p>
+                              <p class="product-description">${producto.topping3}</p>
+                              <span class="price">$ ${producto.precio}</span>
+                              <i class="fa-solid fa-cart-plus add-cart"></i>`;
+                              
+      divShop3.appendChild(divProducto3);
+      triples.appendChild(divShop3);
+    });
+  }
+  IniciarProductos3();
