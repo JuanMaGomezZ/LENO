f229aa1a8068fd4b8d2f1ddbac04ef845566ebee
Juan Manuel Gomez Omil <72764054+JuanchiiGomezZ@users.noreply.github.com>
2022-07-28 10:50:10 -0300

    Add files via upload

--- JavaScript/burgerMenu.js
+++ JavaScript/burgerMenu.js
@@ -16,3 +16,18 @@ crossIcon.onclick = () => {
   crossIcon.classList.remove("active");
   burgerIcon.classList.remove("hide");
 };
+
+
+/* Cart */
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
+/* Open Cart*/
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+/* Close Cart */
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
